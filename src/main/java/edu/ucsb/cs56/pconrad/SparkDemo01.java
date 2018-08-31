eb9c57c0cfe22200d8aab1b5dae65a1d033fb852
Dylan Kupsh <DKupshm@gmail.com>
2018-08-31 09:59:49 -0700

    Update SparkDemo01.java

--- src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
+++ src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
@@ -21,7 +21,7 @@ public class SparkDemo01 {
 	      	
 		String html = "<h1><a href='/hello'>Hello</a> World!</h1>\n" +
     "<p>This web app is powered by \n" +
-    "<a href='https://github.com/DKupshM/sparkjava-01'>this github repo</a></p> n";
+    "<a href='https://github.com/DKupshM/sparkjava-01'>this github repo</a></p>\n";
 
 	      	spark.Spark.get("/", (req, res) -> html);
 		spark.Spark.get("/hello", (req, res) -> "<p><b>Hello, World!</b>  You just clicked the first link on my web app.</p>");
